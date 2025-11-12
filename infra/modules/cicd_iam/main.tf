87e8ef2e594860ae00311f70f09043fd15cac9d9
Lorenzettig7 <lorenzettig7@gmail.com>
2025-11-12 09:09:23 -0500

    ci: install Terraform and fix deploy workflow

--- infra/modules/cicd_iam/main.tf
+++ infra/modules/cicd_iam/main.tf
@@ -6,6 +6,8 @@ locals {
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
+
+
 # 1) GitHub OIDC provider (create once per account; idempotent)
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
